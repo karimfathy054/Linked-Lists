062f07a2d8403605f37c99f196ae9c505d42c854
karimfathy054 <96244656+karimfathy054@users.noreply.github.com>
2022-03-26 14:52:57 +0200

    fixes

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -1,13 +1,19 @@
 package com.company;
 
-public class Llist {
+public class SingleLinkedList {
      node head;
 
+     static class object {
+        int int_data ;
+        public object(int int_data) {
+            this.int_data = int_data;
+        }
+    }
 
      static class node{
-        int data;
+        object data;
         node next;
-        node(int k){
+        node(object k){
             data = k;
             next = null;
         }
@@ -23,7 +29,7 @@ public class Llist {
         }
         return size;
     }
-    public  void insert(int index, int data){
+    public  void addtoindex(int index, object data){
         node new_node = new node(data);
         if(index>=0 && index<= size()){
             if(head == null){
@@ -47,7 +53,7 @@ public class Llist {
         }
 
     }
-    public  void add(int data){
+    public  void add(object data){
         node n2 = new node(data);
         if (head==null){
             head=n2;
@@ -64,23 +70,25 @@ public class Llist {
     }
     public void printList(){
         if(head==null){
-            System.out.println("list is empty,can not be printed");
+            System.out.println("[]");
         }
         else{
             node n = head;
-            System.out.print("linked list:");
-            while(n!= null){
-                System.out.print(n.data + ", ");
+//            System.out.print("linked list:");
+            System.out.print("[");
+            while(n.next!= null){
+                System.out.print(n.data.int_data + ", ");
                 n=n.next;
             }
+            System.out.print(n.data.int_data+"]");
             System.out.println();
         }
 
     }
-    public int get(int index) {
+    public object get(int index) {
         {
             node cursor = head;
-            int x = 0;
+            object x = null;
             if (index >= 0 && index <= size()) {
                 if (head == null) {
                     System.out.println("list is empty");
@@ -98,7 +106,7 @@ public class Llist {
             return x;
         }
     }
-    public  void set (int index,int data) {
+    public  void set (int index,object data) {
 
             node cursor = head;
             if (index >= 0 && index <= size()) {
@@ -144,8 +152,8 @@ public class Llist {
             }
 
     }
-    public  Llist sublist(int fromIndex,int toIndex){
-        Llist sublist = new Llist();
+    public SingleLinkedList sublist(int fromIndex, int toIndex){
+         SingleLinkedList sublist = new SingleLinkedList();
         if(fromIndex>=0 && toIndex>=0 && fromIndex<=size() && toIndex<=size() && fromIndex<=toIndex){
             node cursor =head;
             int i=0;
@@ -160,20 +168,80 @@ public class Llist {
             }
         }
         else{
-            System.out.println("invalid indeces");
+            System.out.println("invalid indexes");
         }
         return sublist;
     }
-    public  boolean contains(int key){
+    public  boolean contains(object key){
         node cursor = head;
         boolean flag=false;
         while(cursor.next != null && !flag ){
-            if(cursor.data == key){
+            if(cursor.data.int_data == key.int_data){
                 flag = true;
             }
             cursor=cursor.next;
         }
         return flag;
+    }
+    public static void main(String[] args) {
+        // write your code here
+        SingleLinkedList list =new SingleLinkedList();
+        list.printList();
+        System.out.println(list.isEmpty());
+        list.add(new object(1));
+        list.add(new object(2));
+        list.add(new object(3));
+        list.add(new object(4));
+        list.add(new object(5));
+        list.printList();
+        SingleLinkedList sublist = list.sublist(2,4);
+        sublist.printList();
+        list.addtoindex(3,new object(8));
+        list.printList();
+        list.set(1,new object(9));
+        System.out.println("does it contain 9??:"+ list.contains(new object(9)));
+        System.out.println("does it contain 12??:"+ list.contains(new object(12)));
+        list.printList();
+        System.out.println(list.isEmpty());
+        list.remove(1);
+        list.printList();
+        object x = list.get(1);
+        System.out.println(x.int_data);
+        list.clear();
+        list.printList();
+        System.out.print(list.isEmpty());
+
+
+
+        /*DoubleLlist list =new DoubleLlist();
+        list.add(new object(1));
+        list.add(new object(2));
+        list.add(new object(3));
+        list.add(new object(4));
+        list.add(new object(5));
+        list.printList();
+        DoubleLlist sublist = list.sublist(2,4);
+        sublist.printList();
+        list.insert(3,new object(8));
+        list.printList();
+        list.set(1,new object(9));
+        System.out.println("does it contain 9??:"+ list.contains(new object(9)));
+        System.out.println("does it contain 12??:"+ list.contains(new object(12)));
+        list.printList();
+        System.out.println(list.isEmpty());
+        list.remove(1);
+        list.printList();
+        int x = list.get(1);
+        System.out.println(x);
+        list.clear();
+        list.printList();
+        System.out.print(list.isEmpty());*/
+
+
+
+
+
+
     }
 }
 
