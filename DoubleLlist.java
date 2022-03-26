5f24badcf3a0d138590334e08ed26a343b388a7b
karimfathy054 <96244656+karimfathy054@users.noreply.github.com>
2022-03-26 14:34:22 +0200

    should work in hackerrank interface

--- DoubleLlist.java
+++ DoubleLlist.java
@@ -3,13 +3,18 @@ package com.company;
 public class DoubleLlist {
     node head;
     node tail;
+    static class object {
+        int int_data ;
+        public object(int int_data) {
+            this.int_data = int_data;
+        }
+    }
 
-
-    static class node{
-        int data;
+    class node{
+        object data;
         node next;
         node prev;
-        node(int k){
+        node(object k){
             data = k;
             next = null;
             prev = null;
@@ -26,7 +31,7 @@ public class DoubleLlist {
         }
         return size;
     }
-    public  void insert(int index, int data){
+    public  void insert(int index, object data){
         node new_node = new node(data);
         if(index>=0 && index<= size()){
             if(head == null){
@@ -54,7 +59,7 @@ public class DoubleLlist {
         }
 
     }
-    public  void add(int data){
+    public  void add(object data){
         node n2 = new node(data);
         if (head==null && tail==null){
             head=n2;
@@ -77,7 +82,7 @@ public class DoubleLlist {
             node n = head;
             System.out.print("linked list:");
             while(n!= null){
-                System.out.print(n.data + ", ");
+                System.out.print(n.data.int_data + ", ");
                 n=n.next;
             }
             System.out.println();
@@ -97,7 +102,7 @@ public class DoubleLlist {
                         cursor = cursor.next;
                         i++;
                     }
-                    x = cursor.data;
+                    x = cursor.data.int_data;
                 }
             } else {
                 System.out.println("not a valid index");
@@ -105,7 +110,7 @@ public class DoubleLlist {
             return x;
         }
     }
-    public  void set (int index,int data) {
+    public  void set (int index,object data) {
 
         node cursor = head;
         if (index >= 0 && index <= size()) {
@@ -174,15 +179,73 @@ public class DoubleLlist {
         }
         return sublist;
     }
-    public  boolean contains(int key){
+    public  boolean contains(object key){
         node cursor = head;
         boolean flag=false;
         while(cursor.next != null && !flag ){
-            if(cursor.data == key){
+            if(cursor.data.int_data == key.int_data){
                 flag = true;
             }
             cursor=cursor.next;
         }
         return flag;
+    }
+    public static void main(String[] args) {
+        // write your code here
+        /*SingleLinkedList list =new SingleLinkedList();
+        list.add(new object(1));
+        list.add(new object(2));
+        list.add(new object(3));
+        list.add(new object(4));
+        list.add(new object(5));
+        list.printList();
+        SingleLinkedList sublist = list.sublist(2,4);
+        sublist.printList();
+        list.addtoindex(3,new object(8));
+        list.printList();
+        list.set(1,new object(9));
+        System.out.println("does it contain 9??:"+ list.contains(new object(9)));
+        System.out.println("does it contain 12??:"+ list.contains(new object(12)));
+        list.printList();
+        System.out.println(list.isEmpty());
+        list.remove(1);
+        list.printList();
+        int x = list.get(1);
+        System.out.println(x);
+        list.clear();
+        list.printList();
+        System.out.print(list.isEmpty());*/
+
+
+
+        DoubleLlist list =new DoubleLlist();
+        list.add(new object(1));
+        list.add(new object(2));
+        list.add(new object(3));
+        list.add(new object(4));
+        list.add(new object(5));
+        list.printList();
+        DoubleLlist sublist = list.sublist(2,4);
+        sublist.printList();
+        list.insert(3,new object(8));
+        list.printList();
+        list.set(1,new object(9));
+        System.out.println("does it contain 9??:"+ list.contains(new object(9)));
+        System.out.println("does it contain 12??:"+ list.contains(new object(12)));
+        list.printList();
+        System.out.println(list.isEmpty());
+        list.remove(1);
+        list.printList();
+        int x = list.get(1);
+        System.out.println(x);
+        list.clear();
+        list.printList();
+        System.out.print(list.isEmpty());
+
+
+
+
+
+
     }
 }
